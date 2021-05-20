384cd8d6cdf8f5e930286c54e5adc3441d4535c4
DannyBrassil <c17351576@mytudublin.ie>
2021-05-20 13:01:35 +0100

    Create Controller Class

---
+++ SQAfinalAssignment/src/Controller.java
@@ -0,0 +1,15 @@
+
+//class to control operations for application
+public class Controller {
+	
+	
+	public Controller() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Controller();
+	}
+
+}
